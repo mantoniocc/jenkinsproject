c7598a369586a92d6038d57cb9e88a2598682546
Mario Cortes <mcortes.it@outlook.com>
2020-11-01 12:47:43 -0300

    test 3

--- webapp/test/index.js
+++ webapp/test/index.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 describe('Sum', () => {
   it('should handle positive numbers', () => {
-    assert.equal(sum(3, 1),-8);
+    assert.equal(sum(0, 0),0);
   });
   it('should handle negative numbers', () => {
     assert.equal(sum(3, -5), -2);
